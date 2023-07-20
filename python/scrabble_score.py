3ecfb9494a2d9c14b6131da0cf43b5514ac04e22
Hristo Hristov <hr100h@gmail.com>
2023-07-20 17:11:04 +0300

    Create new folder "python" with file - scrabble_score.py

---
+++ python/scrabble_score.py
@@ -0,0 +1,17 @@
+score = {"a": 1, "c": 3, "b": 3, "e": 1, "d": 2, "g": 2, 
+         "f": 4, "i": 1, "h": 4, "k": 5, "j": 8, "m": 3, 
+         "l": 1, "o": 1, "n": 1, "q": 10, "p": 3, "s": 1, 
+         "r": 1, "u": 1, "t": 1, "w": 4, "v": 4, "y": 4, 
+         "x": 8, "z": 10}
+
+def scrabble_score(word):
+  total = 0
+  for l in word:
+    small = l.lower()
+    total += score[small]
+  
+  return total
+
+test_word = "test"
+result = scrabble_score(test_word)
+print(test_word, result)
